a478cab7d872ef7b1898887b206954a360adb1df
Isaac <isaacjqz@gmail.com>
2023-04-19 11:30:23 -0400

    UI-2 Chat List Item

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ChatListItem from "./src/components/ChatListItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>WhatsApp!</Text>
+      <ChatListItem />
+      <ChatListItem />
+      <ChatListItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
